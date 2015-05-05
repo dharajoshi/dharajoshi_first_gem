70b0b247522e88066c23245237e46e46cb5efce2
Dhara Joshi <dhara.joshi.hs@gmail.com>
2015-05-05 14:03:35 +0530

    removed setup method call

--- lib/dharajoshi_first_gem/railtie.rb
+++ lib/dharajoshi_first_gem/railtie.rb
@@ -5,7 +5,7 @@ module LogStasher
     config.dharajoshi_first_gem = ActiveSupport::OrderedOptions.new
 
     initializer :dharajoshi_first_gem do |app|
-      DharajoshiFirstGem.setup(app)
+      puts 'initializer in module'
     end
   end
 end
